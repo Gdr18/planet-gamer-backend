0d8b22a6b511c36df1b6e08162e8af47ac2ae93e
Gdr18 <gador8@gmail.com>
2023-11-14 20:22:44 +0100

    Añadiendo pytz

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from datetime import datetime, timezone
+from datetime import datetime
+from pytz import timezone 
 
 app = Flask(__name__)
 
